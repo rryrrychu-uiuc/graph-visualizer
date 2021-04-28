08f2c08168c98023653f577043a9d63ad09267cb
rryrrychu-uiuc <harryc3@illinois.edu>
2021-04-28 00:43:44 -0400

    Utilized graph_editor class in application

--- include/visualizer/graph_manipulator_app.h
+++ include/visualizer/graph_manipulator_app.h
@@ -7,6 +7,7 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "graph_view.h"
+#include "graph_editor.h"
 
 namespace graph {
     
@@ -23,18 +24,29 @@ public:
     
     void mouseDrag(ci::app::MouseEvent event) override;
 
+    void keyDown(ci::app::KeyEvent event) override;
+
     // provided that you can see the entire UI on your screen.
     const double kWindowSize = 1000;
     const double kMargin = 100;
     
     const float kDefaultRadius;
+    
     const ci::Color kDefaultColor;
     const ci::Color kFontColor;
     const ci::Font kDefaultFont;
 private:
     
     GraphView graph_visualizer_;
+    GraphEditor graph_editor_;
     Graph visualized_graph;
+    
+    char app_setting_;
+    //Change to enums
+    const char kMoveMode = 'M';
+    const char kAddNodeMode = 'N';
+    const char kDeleteMode = 'D';
+    const char kAddEdgeMode = 'E';
     
     void InitializeGraph();
 };
--- src/visualizer/graph_manipulator_app.cpp
+++ src/visualizer/graph_manipulator_app.cpp
@@ -6,26 +6,56 @@ namespace graph {
                 graph_visualizer_(kWindowSize, kMargin),
                 kDefaultFont(ci::Font("Times New Roman", 45)),
                 kDefaultColor(ci::Color("orange")),
-                kFontColor(ci::Color("black")),
-                kDefaultRadius(30.0) {
+                kFontColor(ci::Color("white")),
+                kDefaultRadius(30.0),
+                graph_editor_(kDefaultRadius),
+                app_setting_(kMoveMode){
             ci::app::setWindowSize((int) kWindowSize, (int) kWindowSize);
             InitializeGraph();
-            //TODO: Change this to initalize GraphView
-            //TODO: Instance varaibles shoudl be GraphView, GraphEditor, Graph
         }
         
         void GraphVisualizerApp::draw() {
             graph_visualizer_.Draw(visualized_graph);
+            ci::gl::drawString(std::to_string(app_setting_), vec2(500, 50), kFontColor,
+                               kDefaultFont);
         }
         
         void GraphVisualizerApp::mouseDown(ci::app::MouseEvent event) {
-           graph_visualizer_.MoveNode(&visualized_graph, event.getPos());
+            if(app_setting_ == kMoveMode){
+                graph_visualizer_.MoveNode(&visualized_graph, event.getPos());
+            } else if (app_setting_ == kAddNodeMode) {
+                graph_editor_.CreateNode(&visualized_graph, event.getPos());
+            } else if (app_setting_ == kAddEdgeMode) {
+                graph_editor_.CreateEdge(&visualized_graph, event.getPos());
+            }
         }
 
         void GraphVisualizerApp::mouseDrag(ci::app::MouseEvent event) {
-            graph_visualizer_.MoveNode(&visualized_graph, event.getPos());
+            if(app_setting_ == kMoveMode){
+                graph_visualizer_.MoveNode(&visualized_graph, event.getPos());
+            }
         }
         
+        void GraphVisualizerApp::keyDown(ci::app::KeyEvent event) {
+
+            switch (event.getCode()) {
+                case ci::app::KeyEvent::KEY_m:
+                    app_setting_ = kMoveMode;
+                    break;
+
+                case ci::app::KeyEvent::KEY_n:
+                    app_setting_ = kAddNodeMode;
+                    break;
+                    
+                case ci::app::KeyEvent::KEY_d:
+                    app_setting_ = kDeleteMode;
+                    break;
+
+                case ci::app::KeyEvent::KEY_e:
+                    app_setting_ = kAddEdgeMode;
+                    break;
+            }
+        }
         void GraphVisualizerApp::InitializeGraph() {
             Node n1(10, vec2(200,200));
             Node n2(20, vec2(750,200));
